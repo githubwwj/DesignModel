aa6b1159f7d9c913b351f084d056b57d4e2cd1c4
githubwwj <githubwwj@outlook.com>
2018-05-31 12:56:35 +0800

    新增反射主题

--- src/com/wwj/reflect/Client.java
+++ src/com/wwj/reflect/Client.java
@@ -3,33 +3,48 @@ package com.wwj.reflect;
 public class Client {
 
 	public static void main(String[] args) {
-
-		Student student = new Student();
+		
+		/**
+		 * 万事万物皆对象
+		 * 类也是对象
+		 * 有两种不是面向对象的?
+		 * static 基本数据类型
+		 * int Integer
+		 * float Float
+		 * 
+		 * 类的类对象在程序怎么表示？
+		 */
+		
+		Student student=new Student();
 		student.print();
-
-		// class 也是对象
-		// 类也是实例对象,称为类的类类型,它是java.lang.Class
-		Class c1 = Student.class;
-
-		Class c2 = student.getClass();
-
-		// 官网中 c1 or c2 称为 类类型,它们都表示同一个类的类实例对象
-		System.out.println(c1 == c2);
-
+		
+		//类的类对象第一种表示方式,默认隐含了静态变量
+		Class<Student> c1=Student.class;
+		
+		//第二种表示方式
+		Class<?> c2= student.getClass();
+		
+		//官网中  c1 or c2  表示Student类的类类型 
+		System.out.println(c1==c2);
+		
+		
+		//第三种表示方式
 		try {
-			Class c3 = Class.forName("com.wwj.reflect.Student");
-
-			System.out.println(c1 == c3);
+			Class c3=Class.forName("com.wwj.reflect.Student");
+			System.out.println(c1==c3);
+			
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 
 	}
 
 }
-
-class Student {
-	void print() {
+class Student{
+	void print(){
 		System.out.println("我是一名学生");
 	}
 }
